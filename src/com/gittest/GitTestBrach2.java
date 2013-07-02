3b7c25169ccd9b571bf60b191231654952c301b4
Toni Menendez <tonimenen@gmail.com>
2013-07-02 17:15:56 +0200

    1st commit in brach2

---
+++ src/com/gittest/GitTestBrach2.java
@@ -0,0 +1,14 @@
+package com.gittest;
+
+public class GitTestBrach2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Gittest in brach2 ");
+
+
+	}
+
+}
